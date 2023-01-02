091b7450e40e1b969e3d72af6fe36d547970909e
Christopher Hill <cjameshill@gmail.com>
2023-01-02 09:56:51 +0100

    Updated apollo to get graphql endpoint from env var

--- src/apollo.ts
+++ src/apollo.ts
@@ -7,7 +7,7 @@ import {
 // HTTP connection to the API
 const httpLink = createHttpLink({
     // You should use an absolute URL here
-    uri: "http://localhost:4000/graphql",
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
 });
 
 // Cache implementation
