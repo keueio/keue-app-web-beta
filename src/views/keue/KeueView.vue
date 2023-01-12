34df4d2feaca5350473152bca2b14049a3842919
Christopher Hill <cjameshill@gmail.com>
2023-01-12 15:57:02 +0100

    Updated start page to create default app queue

--- src/views/keue/KeueView.vue
+++ src/views/keue/KeueView.vue
@@ -7,6 +7,7 @@ import { computed, ref, watch } from "vue";
 const route = useRoute();
 const keueId = computed(() => route.params.id);
 const appId = computed(() => route.params.app);
+const keueFullId = computed(() => `${route.params.app}---${route.params.id}`);
 const { result, refetch } = useQuery(
     gql`
         query getKeue($name: String!) {
@@ -17,7 +18,7 @@ const { result, refetch } = useQuery(
         }
     `,
     {
-        name: keueId,
+        name: keueFullId,
     }
 );
 const { mutate: pauseKeue, loading: pauseKeueLoading } = useMutation(
@@ -51,10 +52,12 @@ watch(result, () => {
 watch(isRunningToggle, async (value) => {
     console.log("is running value: ", value);
     if (!value) {
-        await pauseKeue({ input: { name: keueId.value, app: appId.value } });
+        await pauseKeue({
+            input: { name: keueFullId.value, app: appId.value },
+        });
         return refetch();
     }
-    await resumeKeue({ input: { name: keueId.value, app: appId.value } });
+    await resumeKeue({ input: { name: keueFullId.value, app: appId.value } });
     return refetch();
 });
 </script>
@@ -65,13 +68,21 @@ watch(isRunningToggle, async (value) => {
             <div class="flex flex-wrap items-center justify-between -mx-4">
                 <div class="w-full lg:w-auto px-4 mb-14 lg:mb-0">
                     <div class="flex flex-wrap items-center">
-                        <img class="mb-6 md:mb-0 mr-10" src="/uinel-assets/images/dashboard-navigations/icon2.svg"
-                            alt="" />
+                        <img
+                            class="mb-6 md:mb-0 mr-10"
+                            src="/uinel-assets/images/dashboard-navigations/icon2.svg"
+                            alt=""
+                        />
                         <div class="w-full md:w-auto">
-                            <h2 class="text-7xl md:text-8xl font-heading font-bold leading-relaxed">
+                            <h2
+                                class="text-7xl md:text-8xl font-heading font-bold leading-relaxed"
+                            >
                                 Your Keue
                             </h2>
-                            <p v-if="result" class="text-darkBlueGray-400 leading-8">
+                            <p
+                                v-if="result"
+                                class="text-darkBlueGray-400 leading-8"
+                            >
                                 {{ result.keue?.name }}
                             </p>
                         </div>
@@ -80,8 +91,11 @@ watch(isRunningToggle, async (value) => {
                 <div class="w-full lg:w-auto px-4">
                     <div class="flex items-center">
                         <ToggleSwitch v-model="isRunningToggle"></ToggleSwitch>
-                        <a class="inline-block py-4 px-8 text-lg leading-6 text-white font-medium tracking-tighter font-heading text-center bg-primary-500 hover:bg-primary-600 focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 rounded-6xl"
-                            href="#">Settings</a>
+                        <a
+                            class="inline-block py-4 px-8 text-lg leading-6 text-white font-medium tracking-tighter font-heading text-center bg-primary-500 hover:bg-primary-600 focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 rounded-6xl"
+                            href="#"
+                            >Settings</a
+                        >
                     </div>
                 </div>
             </div>
@@ -89,13 +103,20 @@ watch(isRunningToggle, async (value) => {
     </section>
     <section class="py-8 bg-blueGray-50">
         <div class="container px-4 mx-auto">
-            <div class="relative p-10 xl:py-12 xl:px-20 bg-grey-300 overflow-hidden rounded-4xl">
+            <div
+                class="relative p-10 xl:py-12 xl:px-20 bg-grey-300 overflow-hidden rounded-4xl"
+            >
                 <div v-if="result" class="relative z-10 sm:max-w-sm">
-                    <h2 class="mb-8 text-2xl md:text-2xl text-grey-600 font-heading font-semibold">
+                    <h2
+                        class="mb-8 text-2xl md:text-2xl text-grey-600 font-heading font-semibold"
+                    >
                         Status: {{ result.keue?.state }}
                     </h2>
-                    <a :href="ingressUrl" target="_blank"
-                        class="mb-8 text-xl md:text-xl text-grey-600 font-heading font-medium">
+                    <a
+                        :href="ingressUrl"
+                        target="_blank"
+                        class="mb-8 text-xl md:text-xl text-grey-600 font-heading font-medium"
+                    >
                         {{ ingressUrl }}
                     </a>
                 </div>
